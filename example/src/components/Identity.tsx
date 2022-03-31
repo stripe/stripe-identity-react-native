a8edeb85653892490bfca5cc3a5218e01864b92f
Mateusz Skwierczyński <mskw@MacBook-Pro-Mateusz.local>
2022-03-31 11:49:31 +0200

    fix: remove unused testId

--- example/src/components/Identity.tsx
+++ example/src/components/Identity.tsx
@@ -42,7 +42,7 @@ export function Identity({ fetchOptions }: Props) {
   return (
     <View style={styles.container}>
       <View style={styles.buttonContainer}>{renderButton()}</View>
-      <Text testID="status-text">Status: {status ?? 'Undefined'}</Text>
+      <Text>Status: {status ?? 'Undefined'}</Text>
     </View>
   );
 }
