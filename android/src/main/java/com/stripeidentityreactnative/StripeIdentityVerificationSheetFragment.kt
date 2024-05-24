ac4505783bcf30eb62f2c72dd6732c37550dfab3
Chen Cen <ccen@stripe.com>
2024-05-24 14:13:22 -0700

    fix image resource in prod

--- android/src/main/java/com/stripeidentityreactnative/StripeIdentityVerificationSheetFragment.kt
+++ android/src/main/java/com/stripeidentityreactnative/StripeIdentityVerificationSheetFragment.kt
@@ -1,10 +1,12 @@
 package com.stripeidentityreactnative
 
+import android.content.ContentResolver
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.URLUtil
 import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
 import com.facebook.react.bridge.Promise
@@ -34,8 +36,9 @@ class StripeIdentityVerificationSheetFragment : Fragment() {
   private fun createIdentityVerificationSheet(): IdentityVerificationSheet {
     verificationSessionId = arguments?.getString("sessionId").orEmpty()
     ephemeralKeySecret = arguments?.getString("ephemeralKeySecret").orEmpty()
-    val imageUri = arguments?.getBundle("brandLogo")?.getString("uri").orEmpty()
-    return IdentityVerificationSheet.create(this, IdentityVerificationSheet.Configuration(brandLogo = Uri.parse(imageUri))) {
+    val imageUriString = arguments?.getBundle("brandLogo")?.getString("uri").orEmpty()
+    return IdentityVerificationSheet.create(this, IdentityVerificationSheet.Configuration
+      (brandLogo = getUrlOrResourceId(imageUriString))) {
       promise?.let { currentPromise ->
         val result = WritableNativeMap()
         when (it) {
@@ -49,6 +52,25 @@ class StripeIdentityVerificationSheetFragment : Fragment() {
       }
 
     }
+  }
+
+  private fun getUrlOrResourceId(uriString: String?): Uri {
+    return uriString?.let {
+      if (URLUtil.isValidUrl(it)) {
+        // Debug mode, Image.resolveAssetSource resolves to local http:// URL
+        Uri.parse(it)
+      } else {
+        // Release mode, Image.resolveAssetSource resolves to a drawable resource
+        val brandLogoId = resources.getIdentifier(it, "drawable", context?.packageName) // int
+        Uri.Builder()
+          .scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
+          .authority(resources.getResourcePackageName(brandLogoId))
+          .appendPath(resources.getResourceTypeName(brandLogoId))
+          .appendPath(resources.getResourceEntryName(brandLogoId))
+          .build()
+
+      }
+    } ?: Uri.parse("")
   }
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
